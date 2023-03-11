c4e51a7e2aad00a3f43beec5a76b39d1dfcb8cb6
Lukas <lukasasaelbacelar@gmail.com>
2023-03-11 12:40:41 -0300

    Exercicise 01,02 and 03 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+    let normal = string.split("");
+    let reverse = [];
+    for (let l, i = 0; i < string.length; i++) {
+        l = normal.pop();
+        reverse.push(l);
+    }
+    reverse =reverse.join("");
+    return reverse;
 };
 
 // Do not edit below this line
